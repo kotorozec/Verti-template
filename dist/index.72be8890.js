b0768ff62f91c0299e0b39f7fbb63950f6f73bf8
Dominik <dominik.kantorowicz@gmail.com>
2022-08-08 12:20:13 +0200

    inital commit

---
+++ dist/index.72be8890.js
@@ -0,0 +1,10 @@
+// ******* Scroll Reveal functions *******
+ScrollReveal().reveal(".headline");
+ScrollReveal().reveal(".tagline", {
+    delay: 500
+});
+ScrollReveal().reveal(".punchline", {
+    delay: 2000
+}); //  ******* Menu functions  *******
+
+//# sourceMappingURL=index.72be8890.js.map
---
+++ js/main.js
@@ -0,0 +1,5 @@
+// ******* Scroll Reveal functions *******
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.tagline', { delay: 500 });
+ScrollReveal().reveal('.punchline', { delay: 2000 });
+//  ******* Menu functions  *******
