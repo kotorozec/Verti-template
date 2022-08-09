1bed6a8e6dfab4daa54b2aa4487f81da8b7be6b0
Dominik <dominik.kantorowicz@gmail.com>
2022-08-09 15:41:53 +0200

    header and menu with burger for mobile

---
+++ dist/index.72be8890.js
@@ -0,0 +1,17 @@
+// ******* Scroll Reveal functions *******
+ScrollReveal().reveal(".headline");
+ScrollReveal().reveal(".tagline", {
+    delay: 500
+});
+ScrollReveal().reveal(".punchline", {
+    delay: 2000
+});
+//  ******* Menu functions  *******
+const mobileNav = document.querySelector(".menu-list");
+const burgerBtn = document.querySelector(".burger-menu");
+burgerBtn.addEventListener("click", function() {
+    mobileNav.classList.toggle("active");
+    burgerBtn.classList.toggle("active");
+});
+
+//# sourceMappingURL=index.72be8890.js.map
--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ ScrollReveal().reveal('.headline');
 ScrollReveal().reveal('.tagline', { delay: 500 });
 ScrollReveal().reveal('.punchline', { delay: 2000 });
 //  ******* Menu functions  *******
+const mobileNav = document.querySelector('.menu-list');
+const burgerBtn = document.querySelector('.burger-menu');
+
+burgerBtn.addEventListener('click', function (){
+    mobileNav.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
+})
