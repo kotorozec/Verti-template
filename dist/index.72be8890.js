70fb149d0917b94e405340a183ed2d216dda6914
Dominik <dominik.kantorowicz@gmail.com>
2022-08-10 17:24:21 +0200

    section banners info

--- dist/index.72be8890.js
+++ dist/index.72be8890.js
@@ -1,9 +1,24 @@
 // ******* Scroll Reveal functions *******
-ScrollReveal().reveal(".header__logo", {
+ScrollReveal().reveal(".banners_first-reveal", {
     delay: 500
 });
-ScrollReveal().reveal(".menu-list__link", {
+ScrollReveal().reveal(".banners_second-reveal", {
     delay: 1000
+});
+ScrollReveal().reveal(".banners_third-reveal", {
+    delay: 1500
+});
+ScrollReveal().reveal(".banners_forth-reveal", {
+    delay: 2000
+});
+ScrollReveal().reveal(".banners__text__intro", {
+    delay: 2500
+});
+ScrollReveal().reveal(".banners__primary-button", {
+    delay: 3000
+});
+ScrollReveal().reveal(".banners__secondary-button", {
+    delay: 3500
 });
 //  ******* Menu functions  *******
 const mobileNav = document.querySelector(".menu-list");
