ab23bf351c0ee5816c09fe0b85642c20a9874720
Dominik <dominik.kantorowicz@gmail.com>
2022-08-11 16:56:17 +0200

    section features

--- dist/index.72be8890.js
+++ dist/index.72be8890.js
@@ -1,24 +1,27 @@
 // ******* Scroll Reveal functions *******
-ScrollReveal().reveal(".banners_first-reveal", {
+ScrollReveal().reveal("#banners_first-reveal", {
     delay: 500
 });
-ScrollReveal().reveal(".banners_second-reveal", {
+ScrollReveal().reveal("#banners_second-reveal", {
     delay: 1000
 });
-ScrollReveal().reveal(".banners_third-reveal", {
+ScrollReveal().reveal("#banners_third-reveal", {
     delay: 1500
 });
-ScrollReveal().reveal(".banners_forth-reveal", {
+ScrollReveal().reveal("#banners_forth-reveal", {
     delay: 2000
 });
-ScrollReveal().reveal(".banners__text__intro", {
+ScrollReveal().reveal("#text__intro", {
     delay: 2500
 });
-ScrollReveal().reveal(".banners__primary-button", {
+ScrollReveal().reveal("#primary-button", {
     delay: 3000
 });
-ScrollReveal().reveal(".banners__secondary-button", {
+ScrollReveal().reveal("#secondary-button", {
     delay: 3500
+});
+ScrollReveal().reveal("#features", {
+    delay: 500
 });
 //  ******* Menu functions  *******
 const mobileNav = document.querySelector(".menu-list");
--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // ******* Scroll Reveal functions *******
-ScrollReveal().reveal('.banners_first-reveal', {delay: 500});
-ScrollReveal().reveal('.banners_second-reveal', {delay: 1000});
-ScrollReveal().reveal('.banners_third-reveal', {delay: 1500});
-ScrollReveal().reveal('.banners_forth-reveal', {delay: 2000});
-ScrollReveal().reveal('.banners__text__intro', {delay: 2500});
-ScrollReveal().reveal('.banners__primary-button', {delay: 3000});
-ScrollReveal().reveal('.banners__secondary-button', {delay: 3500});
+ScrollReveal().reveal('#banners_first-reveal', {delay: 500});
+ScrollReveal().reveal('#banners_second-reveal', {delay: 1000});
+ScrollReveal().reveal('#banners_third-reveal', {delay: 1500});
+ScrollReveal().reveal('#banners_forth-reveal', {delay: 2000});
+ScrollReveal().reveal('#text__intro', {delay: 2500});
+ScrollReveal().reveal('#primary-button', {delay: 3000});
+ScrollReveal().reveal('#secondary-button', {delay: 3500});
+ScrollReveal().reveal('#features', {delay: 500});
+
 
 
 //  ******* Menu functions  *******
